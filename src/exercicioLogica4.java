24120f4932ac0833777c0dd8193192c0277fbe0a
Eduardo Andrade Santos <eandrasa@everis.com>
2020-10-22 15:47:51 -0300

    first commit

---
+++ src/exercicioLogica4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class exercicioLogica4 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		 
+		double a,b,c,delta,x1,x2;
+		
+		System.out.printf("valor a: ");
+		a = (double) ler.nextDouble();
+		System.out.printf("valor b: ");
+		b = (double) ler.nextDouble();
+		System.out.printf("valor c: ");
+		c = (double) ler.nextDouble();
+		
+		delta=((delta=b*b)-(4*a*c));
+		x1=((-b)+(Math.sqrt(delta)))/2*a;
+		x2=((-b)-(Math.sqrt(delta)))/2*a;
+		
+		System.out.println("delta:"+delta);
+		System.out.println("x1:"+x1);
+		System.out.println("x2:"+x2);
+
+		}
+		
+	}
